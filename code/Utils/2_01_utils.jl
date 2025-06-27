61fda647816e637d5aabddf3fe178ecb0cd4cca2
Michaël Lejeune <66875027+Michael-ljn@users.noreply.github.com>
2025-06-27 18:25:55 +1000

    global SOS upload

---
+++ code/Utils/2_01_utils.jl
@@ -0,0 +1,7 @@
+include("./general_utils/config.jl");
+include("./general_utils/ssp_utils.jl");
+
+
+respath = mkpath(config_respath* "2_01_global_space/")*"/";
+config_respath = mkpath(config_respath* "2_01_global_space/")*"/";
+;
--- code/Utils/2_02_utils.jl
+++ code/Utils/2_02_utils.jl
@@ -2,5 +2,5 @@ include("./general_utils/config.jl");
 include("./general_utils/ssp_utils.jl");
 
 
-respath = mkpath(config_respath* "/2_02_allocated_space/");
-config_respath = mkpath(config_respath* "/2_02_allocated_space/");
+respath = mkpath(config_respath* "/2_02_allocated_space/")*"/";
+config_respath = mkpath(config_respath* "/2_02_allocated_space/")*"/";
